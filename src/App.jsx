c6f199b0e7d81afe85cad70e4bb2ba1b0533692a
giorgiadifranco <giorgiadf08@gmail.com>
2024-11-15 17:14:07 +0100

    fix function and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,17 @@
 import { useState } from 'react'
 import AppHeader from './assets/components/AppHeader.jsx'
-//import AppMain from './assets/components/AppMain.jsx'
-import languages from './assets/data/languages.js'
+import AppMain from './assets/components/AppMain.jsx'
+
 
 
 
 function App() {
 
 
-  const [active, setActive] = useState(0)
-
-  function handleClick(e){
-
-    console.log(e.target);
-    const newActive = Number(e.target.getAttribute('data-index'))
-    console.log(newActive);
-    setActive(newActive)
-    
-  }
 
   return (
     <>
       <AppHeader />
-      <main>
-            <div className="container">
-
-            {languages.map((language, index)=>(
-            <div className="accordion" key={language.id}>
-                <button className="lang_btn" onClick={handleClick} data-index={index}>
-                  {language.title}
-                </button>
-            
-
-            <div className={active === index ? 'active' : 'hide'}>
-            <h2>{language.title}</h2>
-            <p>{language.description}</p>
-            </div>
-        </div>
-
-            
-            
-))}
-            
-            </div>
-        </main>
       
       
     </>
--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,10 +1,46 @@
 
+import languages from '../data/languages.js'
+
 
 export default function AppMain(){
 
+    
+  const [active, setActive] = useState(-1)
+
+  function handleClick(e){
+
+    console.log(e.target);
+    const newActive = Number(e.target.getAttribute('data-index'))
+    console.log(newActive);
+    setActive(newActive)
+    
+  }
+
     return(
 
-        
+        <main>
+            <div className="container">
+
+            {languages.map((language, index)=>(
+            <div className="accordion" key={language.id}>
+                <button className="lang_btn" onClick={handleClick} data-index={index}>
+                  {language.title}
+                </button>
+            
+
+            <div className={active === index ? 'active' : 'hide'}>
+            <h2>{language.title}</h2>
+            <p>{language.description}</p>
+            </div>
+        </div>
+
+            
+            
+))}
+            
+            </div>
+        </main>
+      
 
 
     )
